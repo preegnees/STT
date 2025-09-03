ffa422e7704f95dd3de34ce2025bc5aef1c8e8ed
razyapov <radmir.razyapov@paragon.ru>
2025-09-03 11:52:18 +0300

    Initial Commit

---
+++ AI_NOTETests/AI_NOTETests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_NOTETests.swift
+//  AI_NOTETests
+//
+//  Created by Радмир on 03.09.2025.
+//
+
+import Testing
+@testable import AI_NOTE
+
+struct AI_NOTETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
